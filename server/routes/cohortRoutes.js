60ee1eecc09a2bad02bf549cef2be6fc6b430d8d
Cristina Rueda <crueda133@hotmail.com>
2025-01-11 16:33:20 +0100

    routes added

---
+++ server/routes/cohortRoutes.js
@@ -0,0 +1,6 @@
+// // Cohort Routes
+// POST /api/cohorts - Creates a new cohort
+// GET /api/cohorts - Retrieves all of the cohorts in the database collection
+// GET /api/cohorts/:cohortId - Retrieves a specific cohort by id
+// PUT /api/cohorts/:cohortId - Updates a specific cohort by id
+// DELETE /api/cohorts/:cohortId - Deletes a specific cohort by id
